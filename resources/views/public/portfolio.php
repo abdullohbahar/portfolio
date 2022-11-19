59c52b313300b4cda08984e36877f62b0e83b663
bahar <abdullohbahar@gmail.com>
2022-11-20 03:02:00 +0700

    create home page

---
+++ resources/views/public/portfolio.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Baharudin Abdulloh Mun'im | Portfolio</title>
+  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins&display=swap" rel="stylesheet"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="./landing/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#"><b>HOME</b></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Section 1 -->
+  <section class="mb-5">
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4 text-center">
+          <img src="./landing/images/me.png" alt="" srcset="" class="w-100 img-fluid rounded shadow-lg">
+        </div>
+        <div class="sm-12 col-md-8 col-lg-8 col-xl-8 mt-2">
+          <h1><b>Baharudin Abdulloh Mun'im</b></h1>
+          <p>Backend Developer</p>
+          <b>
+            <div class="row">
+              <div class="col-2">
+                <p>AGE</p>
+              </div>
+              <div class="col-10">
+                <p>: 20</p>
+              </div>
+              <div class="col-2">
+                <p>PHONE</p>
+              </div>
+              <div class="col-10">
+                <p>: +6285701223722</p>
+              </div>
+              <div class="col-2">
+                <p>EMAIL</p>
+              </div>
+              <div class="col-10">
+                <p>: <a href="mailto:work.baharudin@gmail.com" class="text-reset text-decoration-none">work.baharudin@gmail.com</a></p>
+              </div>
+              <div class="col-2">
+                <p>ADDRESS</p>
+              </div>
+              <div class="col-10">
+                <p>: Jetis, Bantul, Yogyakarta</p>
+              </div>
+              <div class="col-12 mt-2">
+                <h3>
+                  <a href="https://github.com/abdullohbahar" target="_blank" class="text-reset text-decoration-none">
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                  <a href="https://gitlab.com/AbdullohBahar" target="_blank" class="text-reset text-decoration-none">
+                    <i class="fa-brands fa-gitlab"></i>
+                  </a>
+                  <a href="https://www.linkedin.com/in/baharudin-abdulloh-mun-im-713158177/" target="_blank" class="text-reset text-decoration-none">
+                    <i class="fa-brands fa-linkedin"></i>
+                  </a>
+                  <a href="https://www.instagram.com/baharudin_abdulloh_m/" target="_blank" class="text-reset text-decoration-none">
+                    <i class="fa-brands fa-instagram"></i>
+                  </a>
+                </h3>
+              </div>
+            </div>
+          </b>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <hr>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
